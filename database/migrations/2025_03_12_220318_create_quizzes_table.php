6898758cb29bd94c9737084769fcabbec74e46c3
pnkjpro <pp.pankaj999@gmail.com>
2025-03-13 04:46:30 +0530

    setting up necessary migrations

---
+++ database/migrations/2025_03_12_220318_create_quizzes_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('quizzes', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('category_id')->constrained('categories')->onDelete('cascade');
+            $table->integer('node_id');
+            $table->string('title');
+            $table->text('description');
+            $table->string('banner_image')->nullable();
+            $table->json('quizContents');
+            $table->integer('spot_limit');
+            $table->integer('entry_fees');
+            $table->integer('prize_money');
+            $table->enum('is_active', [0, 1])->default(1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * quizContents contains questions with corresponding options
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('quizzes');
+    }
+};
---
+++ database/migrations/2025_03_12_220425_create_categories_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('slug');
+            $table->text('description');
+            $table->string('icon')->nullable();
+            $table->string('icon_color')->nullable();
+            $table->string('banner_image')->nullable();
+            $table->enum('is_active', [0,1])->default(1);
+            $table->integer('display_order');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('categories');
+    }
+};
