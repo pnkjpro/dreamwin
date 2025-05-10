65604823a9b55ea73a1ebf6277714fde02213de3
pnkjpro <pp.pankaj999@gmail.com>
2025-05-10 12:27:33 +0530

    fully implemented with all necessary changes of razorpay

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -103,7 +103,7 @@ class TransactionController extends Controller
                             ->leftjoin('users as u', function($join){
                                     $join->on('u.id', '=', 'ft.user_id');
                             })
-                            ->select('ft.id','ft.user_id','u.name', 'u.upi_id', 'ft.action', 'ft.amount', 'ft.transaction_id', 'ft.description', 'ft.approved_status')
+                            ->select('ft.id','ft.user_id','u.name', 'u.upi_id', 'ft.action', 'ft.amount', 'ft.razorpay_order_id', 'ft.description', 'ft.approved_status')
                             ->orderBy('ft.id','DESC');
         $totalCount = $transactionsQuery->count();
         $transactions = $transactionsQuery->limit($limit)->offset($offset)->get();
